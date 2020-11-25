55a3f0ae3ba18bc95a1e863ec52ac5b576f8995f
Erika Hirata <rkaisomura@gmail.com>
2020-11-25 18:05:35 +0100

    Creating the navigation bar. Created animation of the icons.

--- js/app.js
+++ js/app.js
@@ -34,40 +34,38 @@
 */
 
 // Build the navigation bar
-const hrefList = ['#hero', '#rings', '#earrings', '#bracelets', '#watches','#shop'];
-const textList = ['Home', 'Rings', 'Earrings', 'Bracelets', 'Watches', 'Shop'];
 
-for (let i=0; i < hrefList.length; i++) {
+const textList = ['Rings', 'Earrings', 'Bracelets', 'Watches', 'Shop'];
+
+const listLinks = document.getElementsByTagName('section');        
+for (let i=0; i < listLinks.length; i++) {
     const unorderedList = document.querySelector('#navbar__list');
     const listItem = document.createElement('li');
     unorderedList.appendChild(listItem);
     const anchorTag = document.createElement('a');
     anchorTag.id = "link";
-    anchorTag.setAttribute('href', hrefList[i]);
+    anchorTag.setAttribute('href', "#"+listLinks[i].id);
     anchorTag.textContent = textList[i];
     listItem.appendChild(anchorTag);
 }
 
-// Top button appears when user scroll down more than 50px
-const topButton = document.getElementById("topBtn");
+    
 
-window.onscroll = function() {
-    scrollPage()
-};
+// Add class 'active' to links when near of viewport
+window.addEventListener('scroll', function(){
+    const navLinks = document.querySelectorAll('section');
+    const fromTop = window.scrollY;
 
-function scrollPage(){
-    if (document.documentElement.scrollTop > 50){
-        topButton.style.display = "block";
-    } else {
-        topButton.style.display = "none";
-    }
-}
-// When user click on the Top button, the page goes to the top
-function topPage(){
-    document.documentElement.scrollTop = 0;
-}
+    navLinks.forEach(function() {
+        let section = document.querySelector();
 
-// Add class 'active' to section when near top of viewport
+        if () {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+});
 
 
 // Scroll to anchor ID using scrollTO event
@@ -84,4 +82,38 @@ function topPage(){
 // Scroll to section on link click
 
 // Set sections as active
+
+// Animation of the watch and diamond icons
+function iconanim(){
+    let iconAnimation = document.getElementById('icon-animation');
+    iconAnimation.innerHTML = "&#xf017;";
+
+    setTimeout(function(){
+        iconAnimation.innerHTML = "&#xf219;";
+    }, 1000);
+}
+
+iconanim();
+setInterval(iconanim, 2000);
+
+// Top button appears when user scroll down more than 100px. Used tutorial from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+const topButton = document.getElementById("topBtn");
+
+window.onscroll = function() {
+    scrollPage()
+};
+
+function scrollPage(){
+    if (document.documentElement.scrollTop > 100){
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+// When user click on the Top button, the page goes to the top
+function topPage(){
+    document.documentElement.scrollTop = 0;
+}
+
 
